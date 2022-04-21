eee3a1dd6a60f133758a01b26c8b7c8598433e9d
Tima02121991 <mirzakaev_timur@mail.ru>
2022-04-21 22:35:06 +0500

    Valid test

---
+++ src/test/java/ru/netology/ApiPostman.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiPostman {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Believe me") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Believe me"))
+        ;
+    }
+}
